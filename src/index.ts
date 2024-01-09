6b17d1f28cc1bba976dc596cf4545949f7528fe2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2024-01-09 11:10:09 +0100

    Add publish action

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { Cond, Condition, ConditionValue } from "./Condition";
+export { Fn, Function } from "./Function";
+export { ISequelizable, ISerializable, Q, Query, SQLFlavor, SelectQuery } from "./Query";
