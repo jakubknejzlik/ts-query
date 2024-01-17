10e60ad1a8ea20b64293543656a230fe8d32fc57
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2024-01-17 11:14:01 +0100

    Add storybook and github pages publish

---
+++ src/QueryPreview.tsx
@@ -0,0 +1,37 @@
+// import * as Antd from "antd";
+import { themes } from "prism-react-renderer";
+import React, { useState } from "react";
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
+import * as components from "./index";
+
+interface QueryPreviewProps {
+  code: string;
+}
+
+export const QueryPreview = ({ code }: QueryPreviewProps) => {
+  const [state, setState] = useState<{ code: string }>({ code: code.trim() });
+
+  return (
+    <>
+      <LiveProvider
+        code={`const query = ${state.code};\nrender(<>{query.toSQL(Q.flavors.mysql)}</>)`}
+        scope={{
+          ...components,
+        }}
+        noInline={true}
+      >
+        <div>
+          <LiveEditor
+            code={state.code}
+            onChange={async (code) => setState({ code })}
+            theme={themes.dracula}
+          />
+        </div>
+        <div style={{ padding: 8, background: "#EEE", marginBottom: 16 }}>
+          <LiveError />
+          <LivePreview />
+        </div>
+      </LiveProvider>
+    </>
+  );
+};
