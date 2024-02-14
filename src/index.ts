b1e1b1f3b3007a6ecadeaacb84fb093cfdc9723b
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2024-02-14 15:02:47 +0100

    Fix build

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,7 @@ export { DeleteMutation, InsertMutation, UpdateMutation } from "./Mutation";
 export { AWSTimestreamFlavor } from "./flavors/aws-timestream";
 export { MySQLFlavor } from "./flavors/mysql";
 
-export {
-  Cond,
-  ICondition as Condition,
-  type ConditionValue,
-} from "./Condition";
+export { Cond, Condition, type ConditionValue } from "./Condition";
 export { Fn, Function } from "./Function";
 export { Q, Query, SelectQuery } from "./Query";
 export {
